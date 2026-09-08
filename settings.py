1957537adfc02ce2206b39d78cbad5b7776469dd
deepthikovvuri <deepthireddykovvuri3627@gmail.com>
2026-09-08 20:42:31 +0530

    Connect Django to PostgreSQL

--- settings.py
+++ settings.py
@@ -1,3 +1,4 @@
+import dj_database_url
 """
 Django settings for event_management project.
 
@@ -70,10 +71,9 @@ WSGI_APPLICATION = 'wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///' + str(BASE_DIR / 'db.sqlite3')
+    )
 }
 
 
